94b3da4c0654ca736a0a022a1395b1116bec8ed2
tcnksm <nsd22843@gmail.com>
2016-01-13 21:46:13 +0900

    Remove unused file

--- error.go
+++
@@ -1,9 +0,0 @@
-package input
-
-import "errors"
-
-var (
-	ErrEmpty      = errors.New("default value is not provided but input is empty")
-	ErrNotNumber  = errors.New("input must be number")
-	ErrOutOfRange = errors.New("input is out of range")
-)
